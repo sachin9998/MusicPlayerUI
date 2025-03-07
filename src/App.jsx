bf4f80d66e7293deea0316d2000f87ccfc351ed1
Sachin Alam <sachinalam4216@gmail.com>
2025-03-07 14:41:00 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,30 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="bg-black text-white">
-      <nav>
-        <div>
-          <img src="" alt="" />
-        </div>
+    <div className="bg-black text-white vw-100 vh-100">
+      <main className="p-4 d-flex">
+        <nav className="nav-bar">
+          <div>
+            <img src="/Logo.svg" alt="" />
+          </div>
+
+          <div>
+            <ul className="d-flex flex-column gap-3 list-unstyled nav-links mt-4">
+              <li>For You</li>
+              <li>Top Tracks</li>
+              <li>Favourites</li>
+              <li>Recently Played</li>
+            </ul>
+          </div>
+        </nav>
 
         <div>
-          <ul>
-            <li>For You</li>
-            <li>For You</li>
-            <li>For You</li>
-          </ul>
+          <h2>For You</h2>
+
+          <input type="text" />
         </div>
-      </nav>
+      </main>
     </div>
   );
 }
